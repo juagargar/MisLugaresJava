bed2715f4c6e76c69fd56e03456ad0c8a3df6e2b
Juancri <juancri@juancristobal.almudeyne@gmail.com>
2016-05-26 13:39:38 +0200

    Initial commit

---
+++ mislugares/src/main/java/org/example/mislugares/GeoPunto.java
@@ -0,0 +1,56 @@
+package org.example.mislugares;
+
+/**
+ * Created by juancri on 24/05/16.
+ */
+public class GeoPunto {
+
+
+
+    private double longitud, latitud;
+
+
+
+    public GeoPunto(double longitud, double latitud) {
+
+        this.longitud= longitud;
+
+        this.latitud= latitud;
+
+    }
+
+
+
+    public String toString() {
+
+        return new String("longitud:" + longitud + ", latitud:"+ latitud);
+
+    }
+
+
+
+    public double distancia(GeoPunto punto) {
+
+        final double RADIO_TIERRA = 6371000; // en metros
+
+        double dLat = Math.toRadians(latitud - punto.latitud);
+
+        double dLon = Math.toRadians(longitud - punto.longitud);
+
+        double lat1 = Math.toRadians(punto.latitud);
+
+        double lat2 = Math.toRadians(latitud);
+
+        double a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+
+                Math.sin(dLon/2) * Math.sin(dLon/2) *
+
+                        Math.cos(lat1) * Math.cos(lat2);
+
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+
+        return c * RADIO_TIERRA;
+
+    }
+
+}
---
+++ mislugares/src/main/java/org/example/mislugares/Principal.java
@@ -0,0 +1,21 @@
+package org.example.mislugares;
+
+/**
+ * Created by juancri on 24/05/16.
+ */
+public class Principal {
+    public static void main(String[] main) {
+
+        Lugar lugar = new Lugar("Escuela Politécnica Superior de Gandía",
+
+                "C/ Paranimf, 1 46730 Gandia (SPAIN)", -0.166093, 38.995656,
+
+                962849300, "http://www.epsg.upv.es",
+
+                "Uno de los mejores lugares para formarse.", 3);
+
+        System.out.println("Lugar " + lugar.toString());
+
+    }
+
+} //Todo hacer la practica
